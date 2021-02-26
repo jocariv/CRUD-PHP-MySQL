d34968bedea044a4901f4cbbb3b87162991b4ed4
jocariv <66138235+jocariv@users.noreply.github.com>
2021-02-26 02:51:25 +0000

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -2,7 +2,7 @@
     session_start(); #Inicia una secion o reanuda una exixtente
     $servername = "172.16.1.6";   #mmLocalhost o IP
     $username = "userku";          #Usuario de la dB
-    $password = "Limaperu-2020";   #Contraseña de la dB
+    $password = "Limaperu-2021";   #Contraseña de la dB
     $database = "sistema";       #Nombre de la db
     $port = "3306";              #puerto por el que se conecta la dB
     $conn = mysqli_connect($servername, $username, $password, $database, $port);
