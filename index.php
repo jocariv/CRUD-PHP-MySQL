032dd9b1ea4e815240689739d0a3bfa102280fdc
jocariv <66138235+jocariv@users.noreply.github.com>
2021-03-08 14:08:52 +0000

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>PHP MsSQL CRUD SERVER DEV 2</title>
+    <title>PHP MsSQL CRUD SERVER DEV ENV</title>
     <!-- CDN de Bootstrap 4-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -27,7 +27,8 @@
     <div class="container">
         <nav class="navbar navbar-dark bg-dark">
             <div class="container">
-                <a href="index.php" class="navbar-brand">PHP MySQL CRUD SERVER DEV</a>
+                <a href="index.php" class="navbar-brand">PHP MySQL CRUD SERVER 
+                </a>
             </div>
         </nav>
         <div class="container p-4">
